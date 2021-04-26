c051389f84462a960251e2c255a87490cecdf41f
Vladimir Popov <vlad@2pvs.com>
2021-04-26 19:34:59 +0300

    print

--- build.sh
+++ build.sh
@@ -4,11 +4,12 @@ for LAMBDA in Launcher Reducer Worker
 do
     echo $LAMBDA
     cd lambdas/$LAMBDA
+    sudo mkdir libs/
     if [ $LAMBDA = "Worker" ]; then
         python3.8 -m pip install Pillow -t .
-        cp -R ../../libs/images .
+        sudo cp -R ../../libs/images libs/images/
     fi
-    cp -R ../../libs/utils .
+    sudo cp -R ../../libs/utils libs/utils/
     zip -r $LAMBDA.zip . 
     aws lambda update-function-code --function-name $LAMBDA --zip-file fileb://$LAMBDA.zip --region $AWS_REGION --profile $AWS_PROFILE
     rm $LAMBDA.zip
--- lambdas/Worker/Worker.py
+++ lambdas/Worker/Worker.py
@@ -6,6 +6,7 @@ def lambda_handler(event, context):
     queue = ut.Queue()
     for i, m in enumerate(evt.messages):
         msg = ut.QueueMessage(m)
+        print(msg)
         source_file =  msg.read_obj_from_s3()
         dest_file = im.convert_jpeg(source_file)
         copy_status = msg.save_obj_to_s3(dest_file) 
